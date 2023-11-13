0aa55f5f125c15966218d510a27f37af0fa19099
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-11-13 12:41:39 +0200

    create the routes for the app for login and home pages

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,12 @@
-import classes from "./Login.module.css"
+import classes from "./Login.module.css";
+import loginImage from "../../Images/login-image.png";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <img src={loginImage} alt="login-img" />
+    </div>
+  );
 };
 
 export default Login;
