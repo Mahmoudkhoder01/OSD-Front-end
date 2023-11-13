f68c6a2ba678523764c70d6bb55ec14fd5e277b3
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-11-13 16:40:31 +0200

    add the wuote with the animation of showing the close icon

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 import Nav from "../../Components/Nav/Nav";
+import Quote from "../../Components/Quote/Quote";
 import classes from "./Home.module.css";
 
 const Home = () => {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <Quote />
+    </>
+  );
 };
 
 export default Home;
