9e7fff182da587807559cae7fdc780eb6d5eda56
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-11-27 12:02:56 +0200

    finish the logout functionality

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,14 +1,32 @@
 import classes from "./Profile.module.css";
 import profile from "../../Images/profile.webp";
+import { IoIosLogOut } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate = useNavigate();
+
   const storedUser = localStorage.getItem("user");
 
   const userInfo = JSON.parse(storedUser);
 
+  const handleLogOut = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    navigate("/login")
+  };
+
   return (
-    <div className={classes.profile}>
-      <img src={profile} alt="profile-img" className={classes.profile} />
+    <div className={classes.profileContainer}>
+      <div className={classes.profile}>
+        <img src={profile} alt="profile-img" className={classes.profileImg} />
+        <div>
+          <div>{userInfo.email}</div>
+          <div className={classes.logOut} onClick={handleLogOut}>
+            Log Out <IoIosLogOut />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
